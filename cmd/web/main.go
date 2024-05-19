b6b4ffe38decaec03bc764cea68369e26068d5cf
Ashton Esparza <ashton.esparza@protonmail.com>
2024-05-18 21:39:36 -0400

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort = "80";
+
+func main() {
+	// connect to the database
+
+	// create sessions
+
+	// create channels
+
+	// create waitgroup
+
+	// set up the applicatoin config
+
+	// set up mail
+
+	// listen for web connections
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ashton-esparza/concurrent-subscription-service.git
+
+go 1.22.3
